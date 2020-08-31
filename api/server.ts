f79060c6b1a80290d026beedc5cc7276b2c6dd14
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-31 16:54:04 -0500

    Se agrego configuracion de entorno en config/index.ts

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,18 @@
-import express = require('express');
-// Create a new express app instance
+import express from 'express';
+import { config } from '../config';
+import helmet from 'helmet';
+import cors from 'cors';
+
 const app: express.Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('hola');
-  console.log('App is listening on port 3000!');
+
+app.listen(config.api.port, () => {
+  console.log(`Escuchando en el puerto ${config.api.port}`);
 });
---
+++ config/index.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const config = {
+  api: {
+    port: process.env.PORT || 3000,
+    cors: process.env.CORS || '*',
+  },
+  dev: process.env.NODE_ENV !== 'production',
+};
