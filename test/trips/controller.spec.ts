d1f670b84c51b7dcff9ee35c2cee28356efab219
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-01 18:40:25 -0500

    se agregaron prubas con filtros para el controller en  findAll

--- test/trips/controller.spec.ts
+++ test/trips/controller.spec.ts
@@ -8,4 +8,23 @@ describe('controller - trips', () => {
       expect(trips[0]).toStrictEqual(tripsMock[0]);
     });
   });
+  test('Should return one trip with filter city', () => {
+    return Controller.findAll({ limit: 1, city: 'Bogotá' }).then(
+      (trips: string[]) => {
+        expect(trips[0]).toStrictEqual(tripsMock[0]);
+      }
+    );
+  });
+  test('Should return one trip with filter country', () => {
+    return Controller.findAll({ limit: 1, country: 'Colombia' }).then(
+      (trips: string[]) => {
+        expect(trips[0]).toStrictEqual(tripsMock[0]);
+      }
+    );
+  });
+  test('Should return one trip with page = 2 ', () => {
+    return Controller.findAll({ limit: 1, page: 2 }).then((trips: string[]) => {
+      expect(trips[0]).toStrictEqual(tripsMock[1]);
+    });
+  });
 });
