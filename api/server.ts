d07db52e19f0f1001f83f38691fded1c341ed2c1
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-01 16:20:56 -0500

    se cargaron las rutas de trips en la API

--- api/server.ts
+++ api/server.ts
@@ -6,17 +6,16 @@ import cors from 'cors';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDoc from './swagger.json';
 
-const app: express.Application = express();
+import trips = require('./components/trips/network.ts');
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 // Routing
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use('/trips', trips);
 
 app.listen(config.api.port, () => {
   console.log(`Escuchando en el puerto ${config.api.port}`);
