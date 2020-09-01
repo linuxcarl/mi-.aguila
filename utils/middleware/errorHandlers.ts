01b4652691c79b1ebe88631beea004a8a9cfe0f0
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-31 23:57:38 -0500

    se agrego middleware para manejo de errores con boom

---
+++ utils/middleware/errorHandlers.ts
@@ -0,0 +1,28 @@
+import * as boom from '@hapi/boom';
+import { config } from '../../../config/index';
+
+function withErrorStack(error: object, stack: string): object {
+  if (config.dev) {
+    return { ...error, stack };
+  }
+  return error;
+}
+
+export function logErrors(error: any, req: any, res: any, next: any): void {
+  next(error);
+}
+
+export function wrapErrors(err: any, req: any, res: any, next: any): void {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+  next(err);
+}
+
+export function errorHandler(err: any, req: any, res: any, next: any): void {
+  const {
+    outpout: { statusCode, payload },
+  } = err;
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
+}
