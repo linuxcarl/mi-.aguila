70566064b57377b45a2af2f10e74caf04111f97f
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-02 00:57:53 -0500

    se agregaron pruebas en network para peticiones get de trips

--- test/trips/network.spec.ts
+++ test/trips/network.spec.ts
@@ -9,5 +9,33 @@ describe('routes - trips', function () {
     test('should respond with status 200', function (done) {
       request.get('/').expect(200, done);
     });
+    test('should respond with status 200 with params city=Medellin', function (done) {
+      request.get('/?city=Medellin').expect(200, done);
+    });
+
+    test('should respond with status 200 with params country=Colombia', function (done) {
+      request.get('/?country=Colombia').expect(200, done);
+    });
+
+    test('should respond with status 200 with params limit=1', function (done) {
+      request.get('/?limit=1').expect(200, done);
+    });
+
+    test('should respond with status 200 with params page=2', function (done) {
+      request.get('/?page=2').expect(200, done);
+    });
+  });
+
+  describe('GET /trips/total', function () {
+    test('should respond with status 200', function (done) {
+      request.get('/total').expect(200, done);
+    });
+    test('should respond with status 200 with params city=Medellin', function (done) {
+      request.get('/total/?city=Medellin').expect(200, done);
+    });
+
+    test('should respond with status 200 with params country=Colombia', function (done) {
+      request.get('/total/?country=Colombia').expect(200, done);
+    });
   });
 });
